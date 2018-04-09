3c8c2a52a27b82b9103e34cecf1818bf0dbd832e
Unicar <weijia@baseup.com.au>
2018-04-09 18:34:23 +1000

    Added indexPage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import '../layouts/index.css'
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
-
-export default IndexPage
+const IndexPage = ({data}) => {
+  console.log(data)
+  
+  return (
+    <div>
+      {data.allMarkdownRemark.edges.map(({node}) => (
+        <div key={node.id} className="article-box">
+          <h3 className="title">{node.frontmatter.title}</h3>
+          <p className="author">{node.frontmatter.author}</p>
+          <p className="date">{node.frontmatter.date} {node.timeToRead}min read</p>
+          <p className="excerpt">{node.excerpt}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
